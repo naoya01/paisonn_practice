ca559dd9b5cdbc47cdb86ce4c4059b53a8f4b632
naoya01 <naoya.take10.01@icloud.com>
2022-02-23 11:44:10 +0900

    python

--- dwc-py/numpy_answer.py
+++ dwc-py/numpy_answer.py
@@ -5,3 +5,18 @@ print(a)
 print(a.shape)
 print(type(a))
 print(a.dtype)
+
+data = np.zeros(8)
+data[4] = 1
+print(data)
+
+data = np.arange(10,50)
+print(data)
+
+data = np.arange(40)
+data = data[::-1]
+print(data)
+
+l = np.arange(10)
+print(l)
+print(l[4:8])
