2d5a28d6b50a8d00ebf6d7713c9550bab492ff95
naoya01 <naoya.take10.01@icloud.com>
2022-03-04 09:46:07 +0900

    python

--- dwc-py/machine_learning_answer.py
+++ dwc-py/machine_learning_answer.py
@@ -12,3 +12,11 @@ warnings.simplefilter('ignore')
 # 必要な関数を読み込む
 from sklearn.model_selection import train_test_split # 訓練データと検証データに分ける関数
 from sklearn.metrics import confusion_matrix, accuracy_score # 混同行列, 予測正解率を求める関数
+
+titanic_data = pd.read_csv(filepath_or_buffer='https://wals.s3-ap-northeast-1.amazonaws.com/curriculum/ai/titanic3.csv',
+                          sep=',', dtype={'pclass':'str'})
+
+len(titanic_data)
+len(titanic_data.columns)
+titanic_data.head()
+print(titanic_data.head())
